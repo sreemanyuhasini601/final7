78fab3d8a1ef2ac1e4129d78a9ec0642d68c49b8
sreemanyuhasini601 <mhasini2006@gmail.com>
2025-11-15 12:24:13 +0530

    Update samplename.java

--- samplename.java
+++ samplename.java
@@ -1,6 +1,6 @@
 public class samplename {
     public static void main(String[] args) {
         String name="hasini";
-        System.out.println("Hello my name is",+name);
+        System.out.println("Hello my name is"+name);
     }
 }
